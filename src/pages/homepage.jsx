2bef6c91dc88968ad2814b2a585892a9b2750621
lkaldyne <l.kaldyne@gmail.com>
2019-06-14 13:41:46 -0400

    Added AddItem and DeleteItem functionality

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Button, Input, Container, Row, Col } from 'reactstrap';
+import { Button, Input, Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
 
 export class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            buttonText: 'submit'
+            itemList: []
         }
     }
 
-    toggleButton = () => {
-        this.setState({ buttonText: 'laith' })
+    clearBox = () => {
+        document.getElementById("exampletext").value = "";
+    }
+
+    clickSubmit = () => {
+        let text = document.getElementById("exampletext").value
+        console.log(text)
+        this.addItem({
+            text
+        })
+        this.clearBox()
+    }
+
+    addItem = (item) => {
+        item.id = Math.random().toString();
+        let { itemList } = this.state
+        itemList.push(item);
+        this.setState({ itemList })
+    };
+    
+    removeItem = (itemID) => {
+        let { itemList } = this.state
+        itemList = itemList.filter((item) => {
+            return itemID !== item.id;
+        })
+        this.setState({ itemList })
+    }
+
+    handleRemove = (e) => {
+        const id = e.target.value
+        this.removeItem(id)
     }
 
     render() {
@@ -21,7 +50,21 @@ export class HomePage extends React.Component {
                         <Input type="text" name="text" id="exampletext" placeholder="Enter Some Text" />
                     </Col>
                     <Col sm={6} md = {4}>
-                        <Button color="secondary" outline={ false } onClick={this.toggleButton}>{this.state.buttonText}</Button>
+                        <Button color="secondary" outline={ false } onClick={this.clickSubmit}>Submit</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6} md={8}>
+                        <ListGroup>
+                            {this.state.itemList.map((item) => {
+                                return (
+                                    <ListGroupItem key={item.id}>
+                                        {item.text}
+                                        <Button style={{marginLeft:"20px"}} color="danger" value={item.id} onClick={this.handleRemove}>remove</Button>
+                                    </ListGroupItem>
+                                )
+                            })}
+                        </ListGroup>
                     </Col>
                 </Row>
             </Container>
